ca3a66cab50a99c700dd54d3cc4f77629b32ab41
Modern <Modern@MSI>
2023-09-21 21:15:42 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+
+class Program
+{
+    static double S(double a, double b)
+    {
+        double sum = a + b;
+        double result = sum * sum;
+        return result;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите первое число:");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите второе число:");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        double square = S(num1, num2);
+
+        Console.WriteLine($"Ответ: {square}");
+    }
+}
+
+       
